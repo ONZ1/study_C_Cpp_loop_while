dd66e0bb2918b1a5081e7737050e617680d6e52a
ONZ1 <onzi1230@gmail.com>
2021-04-06 00:40:29 +0900

    Create 003loop_1.cpp
    while exercise 001

---
+++ 003loop_while/003loop_while/003loop_1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//1. 구구단 4단, 8단을 출력
+	printf("\n[문제1]\n");
+
+	int section = 4;
+	while (section <= 8)
+	{
+		int multi = 1;
+
+		printf("=====%d단=====\n", section);
+		while (multi <= 9)
+		{
+			printf("%d * %d = %d\n", section, multi, section * multi);
+			multi++;
+		}
+
+		section += 4;
+	}
+
+	fgetc(stdin);
+	return 0;
+}
