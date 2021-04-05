6c87bead6a9dd990dbc994f0ca99c3a9ee1f8cac
ONZ1 <onzi1230@gmail.com>
2021-04-06 01:57:11 +0900

    Update 003loop_1.cpp

--- 003loop_while/003loop_while/003loop_1.cpp
+++ 003loop_while/003loop_while/003loop_1.cpp
@@ -5,19 +5,19 @@ int main(void)
 	//1. 구구단 4단, 8단을 출력
 	printf("\n[문제1]\n");
 
-	int section = 4;
-	while (section <= 8)
+	int section = 4;		//단이 될값을 변수명 section에 저장
+	while (section <= 8)		//section이 8 이하일 때까지 반복
 	{
-		int multi = 1;
+		int multi = 1;		//단 값에 곱할 값을 multi변수명에 저장
 
-		printf("=====%d단=====\n", section);
-		while (multi <= 9)
+		printf("=====%d단=====\n", section);			//=====(section)단=====출력하고 줄바꿈
+		while (multi <= 9)		//곱할 값이 9 이하일 때까지 반복
 		{
-			printf("%d * %d = %d\n", section, multi, section * multi);
-			multi++;
+			printf("%d * %d = %d\n", section, multi, section * multi);		//section * multi = (section * multi)\n 출력
+			multi++;		//multi변수값 1씩 증가
 		}
 
-		section += 4;
+		section += 4;		//한 단 끝나면 단수에 4 더하기
 	}
 
 	fgetc(stdin);
